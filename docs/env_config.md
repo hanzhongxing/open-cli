80688057ca8068aefcc7d2fd5d81508e945410c9
韩忠兴 <hanzhongxing@58.com>
2026-08-18 16:52:37 +0800

    fix

--- docs/env_config.md
+++ docs/env_config.md
@@ -1,1 +1,4 @@
 环境配置 在 /src/config 下
+
+图片转化 生成 ASCII 文本
+https://www.text-image.com/convert/ascii.html
--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
+import fs from 'node:fs';
+import chalk from 'chalk';
 import http from 'node:http';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { config } from './config/index.js';
 import { Command } from 'commander';
 import { createRequire } from 'node:module';
@@ -8,6 +12,8 @@ const require = createRequire(import.meta.url);
 import packageJson from '../package.json' with { type: 'json' };
 import { logger } from './lib/logger.js';
 import { home } from './commands/home.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Node.js 版本检查
 const nodeVersion = process.version.replace('v', '');
@@ -29,6 +35,16 @@ logger.info(`当前环境: ${config.env}`);
 //   // logger.info(`[${config.env.toUpperCase()}] Server running at http://${config.server.host}:${config.server.port}`);
 // });
 
+function showBanner() {
+  console.clear();
+  const bannerPath = path.join(__dirname, 'constant/banner.txt');
+  const bannerText = fs.readFileSync(bannerPath, 'utf-8');
+  console.log(chalk.cyan(bannerText));
+}
+if (process.stdout.isTTY) {
+    await showBanner();
+}
+console.log();
 const program = new Command();
 
 program
