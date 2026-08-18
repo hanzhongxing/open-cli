cf470e85f0fe61d7e3493ef6e9895f22452dd8e2
韩忠兴 <hanzhongxing@58.com>
2026-08-18 11:24:54 +0800

    init

--- src/scripts/hello.ts
+++ src/scripts/hello.ts
@@ -4,16 +4,16 @@ import type { ScriptCommand } from '../types/command.js';
 import pc from 'picocolors';
 
 const command: ScriptCommand = {
-  name:'hello',
+  name:'hi',
   description: '打招呼',
-  usage: 'hello <name>',
+  usage: 'hello/hi <name>',
   aliases: ['hello', 'hi'],
   async run(args) {
     if (!args.length) {
       console.log(pc.red('请输入名字，例如: socket'));
       return;
     }
-    console.log(`Hello, ${args[0]}!`);
+    console.log(`Hello, ${args[0]}! 来了老弟~~~`);
   },
 };
 
