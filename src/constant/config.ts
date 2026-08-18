a290c862283f3752a326d6026c3f620a4d7a1fc3
韩忠兴 <hanzhongxing@58.com>
2026-08-18 14:21:28 +0800

    init

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,3 +1,1 @@
-const home_desc='欢迎使用 CLI Manager！Open CLI 是一个 TypeScript 编写的命令行工具，旨在帮助你管理和执行各种脚本命令。';
-
-export { home_desc };
+export const home_desc='欢迎使用 CLI Manager！Open CLI 是一个 TypeScript 编写的命令行工具，旨在帮助你管理和执行各种脚本命令。';
